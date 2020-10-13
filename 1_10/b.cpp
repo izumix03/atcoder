be3c69776f6fd090a95a100f1d738321633c51be
takahiro izumikawa <takahiro.izumikawa@gmail.com>
2020-10-13 18:39:45 +0900

    00:00 s	2932 KB	495

---
+++ 1_10/b.cpp
@@ -0,0 +1,18 @@
+// http://judge.u-aizu.ac.jp/onlinejudge/finder.jsp?course=ITP1
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_10_B
+#include <stdio.h>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  double a, b, degree;
+
+  scanf("%lf %lf %lf", &a, &b, &degree);
+
+  printf("%.8lf\n", (a * b) / 2 * sin(M_PI * degree / 180));
+  printf("%.8lf\n", (a + b + sqrt((a * a) + (b * b) - (2 * a * b * cos(M_PI * degree / 180)))));
+  printf("%.8lf\n", b * sin(M_PI * degree / 180));
+
+  return 0;
+}

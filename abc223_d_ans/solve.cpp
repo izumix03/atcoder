2fcc62e765bfdead0935efa25938287bdcd0d14b
izumix03 <takahiro.izumikawa@gmail.com>
2021-11-11 09:16:44 +0900

    understanding..

--- abc223_d_ans/solve.cpp
+++ abc223_d_ans/solve.cpp
@@ -3,16 +3,17 @@
 #include <queue>
 
 using namespace std;
+// https://qiita.com/Morifolium/items/6c8f0a188af2f9620db2
 
 vector<int> topological_sort(vector<vector<int>> &graph, vector<int> &indegree, int verticesCnt) {
   // 記録用配列 = ソートされた辺たち
   vector <int> sorted_vertices;
 
-  // 入寺数が0の頂点をキューに追加
+  // 入次数が0の頂点をキューに追加
   queue<int> que;
   for (int i = 0; i < verticesCnt; i++) {
-    if (indegree[i] == 0) {
-      que.push(i);
+    if (indegree[i+1] == 0) {
+      que.push(i+1);
     }
   }
 
@@ -20,14 +21,18 @@ vector<int> topological_sort(vector<vector<int>> &graph, vector<int> &indegree, 
   while (!que.empty()) {
     int v = que.front();
     que.pop();
+//    cout << v << "hoge" << endl;
 
     // 隣接している頂点の入寺数を減らし、0 になればキューに追加
-    for (int i = 0; i < graph[v].size(); i++) {
-      int u  = graph[v][i];
+    for (int u : graph[v]) {
       indegree[u] -= 1;
-
+      if (indegree[u] == 0) que.push(u);
     }
+    // 頂点vを配列の末尾に追加する
+    sorted_vertices.push_back(v);
   }
+
+  return sorted_vertices;
 }
 
 int main() {
@@ -43,6 +48,12 @@ int main() {
     graph[u].push_back(v);
     // 入次数を追加
     indegree[v] += 1;
+  }
+
+  vector<int> sorted_vertices = topological_sort(graph, indegree, verticesCnt);
+
+  for (int sorted_vertice : sorted_vertices) {
+    cout << sorted_vertice << endl;
   }
 
   return 0;

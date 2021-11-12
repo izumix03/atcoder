5692f626858ebafe75ad84f1ec102f6dbedf44ec
takahiro izumikawa <takahiro.izumikawa@gmail.com>
2021-11-12 10:53:39 +0900

    finish!!

--- abc223_d_ans/solve.cpp
+++ abc223_d_ans/solve.cpp
@@ -10,18 +10,17 @@ vector<int> topological_sort(vector<vector<int>> &graph, vector<int> &indegree, 
   vector <int> sorted_vertices;
 
   // 入次数が0の頂点をキューに追加
-  queue<int> que;
+  priority_queue<int, vector<int>, greater<>> que;
   for (int i = 0; i < verticesCnt; i++) {
-    if (indegree[i+1] == 0) {
-      que.push(i+1);
+    if (indegree[i] == 0) {
+      que.push(i);
     }
   }
 
   // キューが空になるまで、操作1 ~ 3
   while (!que.empty()) {
-    int v = que.front();
+    int v = que.top();
     que.pop();
-//    cout << v << "hoge" << endl;
 
     // 隣接している頂点の入寺数を減らし、0 になればキューに追加
     for (int u : graph[v]) {
@@ -45,15 +44,20 @@ int main() {
   for (int i = 0; i < edgeCnt; i++) {
     int u, v;
     cin >> u >> v;
-    graph[u].push_back(v);
+    graph[u-1].push_back(v-1);
     // 入次数を追加
-    indegree[v] += 1;
+    indegree[v-1] += 1;
   }
 
   vector<int> sorted_vertices = topological_sort(graph, indegree, verticesCnt);
 
-  for (int sorted_vertice : sorted_vertices) {
-    cout << sorted_vertice << endl;
+  if (sorted_vertices.size() != verticesCnt) {
+    cout << -1 << endl;
+    return 0;
+  }
+
+  for (int i = 0; i < verticesCnt; ++i) {
+    cout << sorted_vertices[i] + 1 << (i + 1 == verticesCnt ? '\n' : ' ');
   }
 
   return 0;
